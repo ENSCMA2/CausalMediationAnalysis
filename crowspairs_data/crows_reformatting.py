9cfaa112f8e737e6b76c780eec2b75a460597557
Karina Halevy <karina.halevy@gmail.com>
2021-02-12 14:50:11 -0500

    crows pairs filtering

--- crowspairs_data/crows_reformatting.py
+++ crowspairs_data/crows_reformatting.py
@@ -1,4 +1,5 @@
 with open("crows_reformatted.txt", "w") as o:
 	with open("crows_pairs.txt", "r") as p:
 		for line in p.readlines():
-			o.write(line.split(",")[1] + "\n")
+			if "race-color" in line:
+				o.write(",".join(line.split(",")[1:]).split("stereo")[0] + "\n")
